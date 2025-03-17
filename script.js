83ff74d0da87dac514548521aaccce078e3fc174
Vicente <carvalho.vicente0207@gmail.com>
2025-03-17 20:08:24 -0300

    Add transição base

---
+++ script.js
@@ -0,0 +1,14 @@
+function lambext() {
+    let lamb = document.getElementById('lamb')
+    let por = document.getElementById('porsche')
+    // LAMBORGUINI
+    lamb.style.transition = "0.5s"
+    lamb.style.height = "390px"
+    lamb.style.top = "200px"
+
+    // PORSCHE
+    por.style.height = "495px"
+    por.style.left = "1400px"
+    por.style.top = "140px"
+    por.style.transition = "0.5s"
+}
