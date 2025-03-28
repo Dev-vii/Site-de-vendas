c1d5acb14c6cedcf16362d283998cfaa59fb5661
Vicente <carvalho.vicente0207@gmail.com>
2025-03-28 14:09:09 -0300

    Projeto finalizado

--- infocar.js
+++ infocar.js
@@ -1,6 +1,14 @@
-let container = document.querySelector('.container')
+document.addEventListener("DOMContentLoaded", () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const carSelected = urlParams.get("car");
 
-function View(){
-    let New = container.querySelector('.container .CarBox')
-    New.classList.add('active')
-}
+    const allCars = document.querySelectorAll(".CarBox");
+
+    allCars.forEach(car => {
+        if (car.dataset.car === carSelected) {
+            car.classList.add("active");
+        } else {
+            car.style.display = "none"; // esconde os outros
+        }
+    });
+});
