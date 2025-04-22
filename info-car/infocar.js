def883ce3dfe7436aa817be9be97442a92ca1141
Vicente <carvalho.vicente0207@gmail.com>
2025-04-22 20:05:28 -0300

    HTML melhorado

--- info-car/infocar.js
+++ info-car/infocar.js
@@ -5,10 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //ENTENDI
     const allCars = document.querySelectorAll(".CarBox"); // seleciona todos os carros
+    const allModels = document.querySelectorAll(".PhotoBox");
 
     allCars.forEach(car => { // roda uma função para cada carro da tela
         if (car.dataset.car === carSelected) { //Se o carro clicado tiver as info corretas, ele mostra os detalhes
             car.classList.add("active");
         }
-    });
+    });  
 });
