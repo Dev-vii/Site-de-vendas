d3b5f28bbeccf808f922c060ee36bc9a0cdd8402
Vicente <carvalho.vicente0207@gmail.com>
2025-05-25 15:56:51 -0300

    Add interatividade com outras opções de fotos

--- info-car/infocar.js
+++ info-car/infocar.js
@@ -5,11 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //ENTENDI
     const allCars = document.querySelectorAll(".CarBox"); // seleciona todos os carros
-    const allModels = document.querySelectorAll(".PhotoBox");
+
 
     allCars.forEach(car => { // roda uma função para cada carro da tela
         if (car.dataset.car === carSelected) { //Se o carro clicado tiver as info corretas, ele mostra os detalhes
             car.classList.add("active");
         }
-    });  
+    });
+
+    // MUDAR AS IMAGENS DO PRODUTO
+    const otherPhotos = document.querySelectorAll(".ModelOp");
+
+    otherPhotos.forEach(photo => {
+        photo.addEventListener("click", () => {
+            const carBox = photo.closest(".CarBox");
+            const mainImg = carBox.querySelector("[data-main]"); //variável recebe o carro com "data-main" (principal)
+            mainImg.src = photo.src; //parte da função que troca a foto do principal pela foto da box
+        });
+    });
+
 });
