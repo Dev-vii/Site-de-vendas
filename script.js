1468bd9b3b7c3c7f9c87abef62a3c25fd275bd8c
Vicente <carvalho.vicente0207@gmail.com>
2025-06-23 13:03:33 -0300

    Add interatividade com barra de pesquisa

---
+++ script.js
@@ -0,0 +1,31 @@
+// FUNÇÃO DARK MODE
+const buttonDark = document.getElementById('button1')
+const buttonBright = document.getElementById('button2')
+
+buttonDark.addEventListener('click', function () {
+    document.body.classList.add("dark-mode")
+})
+
+buttonBright.addEventListener('click', function () {
+    document.body.classList.remove("dark-mode")
+})
+
+// BARRA DE PESQUISA
+const inputsearch = document.getElementById('searchBar')
+
+inputsearch.addEventListener('input', (event) => {
+    const value = event.target.value
+
+    const cars = document.querySelectorAll('.allOptions .line .CarBox')
+    cars.forEach(item => {
+        if(formatString(item.textContent).indexOf(value) !== -1) {
+            item.style.display = "block"
+        } else {
+            item.style.display = "none"
+        }
+    })
+})
+
+function formatString(value) {
+    return value.toLowerCase().trim()
+}
